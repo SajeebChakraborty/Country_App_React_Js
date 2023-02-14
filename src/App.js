ada223690d008ed8f7a286828f44fc7f82b0f4be
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-02-14 21:37:53 +0600

    async fetch countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
 
-function App() {
+const url="https://restcountries.com/v3.1/all";
+
+export default function App() {
+
+  const [isLoading,setIsLoading]=useState(true);
+  const[error,setError]=useState(null);
+  const[countries,setCountries]=useState([]);
+
+  const fetchData = async(url)=>{
+
+   try{
+
+      setIsLoading(true);
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setCountries(data);
+      setIsLoading(false);
+      setError(false);
+
+      console.log(countries);
+
+   }
+   catch(error)
+   {
+
+      setIsLoading(false);
+      setError(error);
+
+   }
+
+  }
+
+  useEffect(()=>{
+
+    fetchData(url);
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
-
-export default App;
