5fb0f41d5d0a3f04d37a88eb2591eb5713999915
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-02-25 23:18:43 +0600

    view fetch data

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 import {v4 as uuidv4} from 'uuid';
 import Country from './Country';
+import style from './countries.module.css';
 
 const Countries = (props) => {
   return (
-    <section>
+    <section className={style.countries}>
 
         {props.countries.map((country)=>{
 
