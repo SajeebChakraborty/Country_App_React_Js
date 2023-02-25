6e973b3760ced1de59b68caa8553bb28a3f3e79f
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-02-26 00:45:05 +0600

    search country

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,9 @@ const Search = (props) => {
   const [searchText,setSearchText] = useState("");
 
   const handleChange = (e) => {
-    
+
     setSearchText(e.target.value);
+    
   }
   useEffect(()=>{
     props.onSearch(searchText);
