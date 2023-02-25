7974a0967c7f079f78f62babe7f46031e7607a70
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-02-25 23:30:56 +0600

    view fetch data

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
+import style from './country.module.css';
+
 const Country = (props) => {
 
   const {country} = props;
   const {name,flags,capital,population,area} = country;
 
   return (
-    <article>
+
+    <article className={style.country}>
 
         <div>
 
-            <img src={flags.png} alt={name.common}></img>
+            <img src={flags.png} alt={name.common} className={style.flag}></img>
             <h3>Name : {name.common}</h3>
             <h3>Population : {population}</h3>
             <h3>Capital : {capital}</h3>
@@ -19,6 +22,7 @@ const Country = (props) => {
         </div>
       
     </article>
+
   )
 }
 
